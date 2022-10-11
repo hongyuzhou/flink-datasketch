fd39d5b05176c675c3ab9633d562d623ea6370f4
hongyuzhou <hongyuzhou@didiglobal.com>
2022-10-11 23:49:27 +0800

    [flink-datasketch] add Stream Distinct test case without sketch

---
+++ src/main/java/org/apache/flink/benchmark/basics/WithoutSketchBenchMark.java
@@ -0,0 +1,109 @@
+package org.apache.flink.benchmark.basics;
+
+import org.apache.flink.api.common.state.*;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeutils.base.StringSerializer;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.benchmark.data.Tuple3SourceGenerator;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.core.serializer.SetSerializer;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.source.datagen.DataGeneratorSource;
+import org.apache.flink.util.Collector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class WithoutSketchBenchMark {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WithoutSketchBenchMark.class);
+
+    public static void main(String[] args) throws Exception {
+
+        final ParameterTool params = ParameterTool.fromArgs(args);
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment
+                .getExecutionEnvironment();
+        //.createLocalEnvironmentWithWebUI(new Configuration());
+
+        env.setParallelism(4);
+        env.enableCheckpointing(5000, CheckpointingMode.EXACTLY_ONCE);
+
+        long rowsPerSecond = Long.parseLong(params.get("rowsPerSecond", "25000"));
+        long numberOfRows = Long.parseLong(params.get("numberOfRows", "1000000000"));
+
+        DataStream<Tuple3<String, Long, String>> source = env
+                .addSource(new DataGeneratorSource<>(
+                        new Tuple3SourceGenerator(),
+                        rowsPerSecond,
+                        numberOfRows))
+                .returns(new TypeHint<Tuple3<String, Long, String>>() {})
+                .name("source");
+
+        DataStream<Record<Tuple3<String, Long, String>>> estimate = source
+                .keyBy(value -> value.f0)
+                .process(new CountDistinctProcessFunction());
+
+        estimate.print();
+        env.execute();
+    }
+
+    private static class CountDistinctProcessFunction extends KeyedProcessFunction<String, Tuple3<String, Long, String>, Record<Tuple3<String, Long, String>>> {
+
+        private ValueState<Set<String>> distinct;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+
+            ValueStateDescriptor<Set<String>> distinctStateDescriptor
+                    = new ValueStateDescriptor<>("DistinctSetState", new SetSerializer<>(new StringSerializer()));
+            distinct = getRuntimeContext().getState(distinctStateDescriptor);
+        }
+
+        @Override
+        public void processElement(Tuple3<String, Long, String> value, Context ctx, Collector<Record<Tuple3<String, Long, String>>> out) throws Exception {
+
+            Set<String> set = distinct.value();
+            if (set == null) {
+                set = new HashSet<>();
+            }
+            set.add(value.f2);
+
+            distinct.update(set);
+
+            out.collect(new Record<>(value, set.size()));
+        }
+    }
+
+    private static class Record<T> {
+        private T val;
+        private Integer uv = 0;
+
+        public Record(T val, Integer uv) {
+            this.val = val;
+            this.uv = uv;
+        }
+
+        public T getVal() {
+            return val;
+        }
+
+        public Integer getUv() {
+            return uv;
+        }
+
+        @Override
+        public String toString() {
+            return "Record{" +
+                    "val=" + val +
+                    ", uv=" + uv +
+                    '}';
+        }
+    }
+}
---
+++ src/main/java/org/apache/flink/core/serializer/SetSerializer.java
@@ -0,0 +1,143 @@
+package org.apache.flink.core.serializer;
+
+import org.apache.flink.api.common.typeutils.CompositeTypeSerializerSnapshot;
+import org.apache.flink.api.common.typeutils.TypeSerializer;
+import org.apache.flink.api.common.typeutils.TypeSerializerSnapshot;
+import org.apache.flink.core.memory.DataInputView;
+import org.apache.flink.core.memory.DataOutputView;
+import org.apache.flink.util.Preconditions;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetSerializer<K> extends TypeSerializer<Set<K>> {
+
+    private final TypeSerializer<K> keySerializer;
+
+    public SetSerializer(TypeSerializer<K> keySerializer) {
+        this.keySerializer =
+                Preconditions.checkNotNull(keySerializer, "The key serializer cannot be null");
+    }
+
+    public TypeSerializer<K> getKeySerializer() {
+        return keySerializer;
+    }
+
+    @Override
+    public boolean isImmutableType() {
+        return false;
+    }
+
+    @Override
+    public TypeSerializer<Set<K>> duplicate() {
+        TypeSerializer<K> duplicateKeySerializer = keySerializer.duplicate();
+        return (duplicateKeySerializer == keySerializer) ? this : new SetSerializer<>(duplicateKeySerializer);
+    }
+
+    @Override
+    public Set<K> createInstance() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public Set<K> copy(Set<K> from) {
+        Set<K> newSet = new HashSet<>();
+        for (K k: from) {
+            K newKey = keySerializer.copy(k);
+            newSet.add(newKey);
+        }
+        return newSet;
+    }
+
+    @Override
+    public Set<K> copy(Set<K> from, Set<K> reuse) {
+        return copy(from);
+    }
+
+    @Override
+    public int getLength() {
+        return -1;
+    }
+
+    @Override
+    public void serialize(Set<K> record, DataOutputView target) throws IOException {
+        final int size = record.size();
+        target.writeInt(size);
+
+        for (K k : record) {
+            keySerializer.serialize(k, target);
+        }
+    }
+
+    @Override
+    public Set<K> deserialize(DataInputView source) throws IOException {
+        final int size = source.readInt();
+
+        final Set<K> set = new HashSet<>(size);
+        for (int i = 0; i < size; ++i) {
+            K key = keySerializer.deserialize(source);
+            set.add(key);
+        }
+
+        return set;
+    }
+
+    @Override
+    public Set<K> deserialize(Set<K> reuse, DataInputView source) throws IOException {
+        return deserialize(source);
+    }
+
+    @Override
+    public void copy(DataInputView source, DataOutputView target) throws IOException {
+        final int size = source.readInt();
+        target.writeInt(size);
+
+        for (int i = 0; i < size; ++i) {
+            keySerializer.copy(source, target);
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj == this
+                || (obj != null
+                && obj.getClass() == getClass()
+                && keySerializer.equals(((SetSerializer<?>) obj).getKeySerializer()));
+    }
+
+    @Override
+    public int hashCode() {
+        return keySerializer.hashCode();
+    }
+
+    @Override
+    public TypeSerializerSnapshot<Set<K>> snapshotConfiguration() {
+        return new SetSerializerSnapshot<>(this);
+    }
+
+    public final class SetSerializerSnapshot<KEY> extends CompositeTypeSerializerSnapshot<Set<KEY>, SetSerializer<KEY>> {
+
+        private static final int CURRENT_VERSION = 1;
+
+        public SetSerializerSnapshot(SetSerializer<KEY> kSetSerializer) {
+            super(kSetSerializer);
+        }
+
+        @Override
+        protected int getCurrentOuterSnapshotVersion() {
+            return CURRENT_VERSION;
+        }
+
+        @Override
+        protected TypeSerializer<?>[] getNestedSerializers(SetSerializer<KEY> outerSerializer) {
+            return new TypeSerializer<?>[] {outerSerializer.getKeySerializer()};
+        }
+
+        @Override
+        protected SetSerializer<KEY> createOuterSerializerWithNestedSerializers(TypeSerializer<?>[] nestedSerializers) {
+            TypeSerializer<KEY> keySerializer = (TypeSerializer<KEY>) nestedSerializers[0];
+            return new SetSerializer<>(keySerializer);
+        }
+    }
+}
